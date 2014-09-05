282500218a9fd771dc1dd80f01c7fbe93d3c8450
lorenzo21 <lauwarrior@yahoo.ca>
2014-09-05 15:34:56 -0400

    Accueil créé

--- Touristix/App_Start/RouteConfig.cs
+++ Touristix/App_Start/RouteConfig.cs
@@ -13,11 +13,23 @@ namespace Touristix
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            /*routes.MapRoute(
+                "Accueil",
+                "",
+                new { controller = "Acceuil", action = "Index", UrlParameter.Optional }
+                );*/
+
             routes.MapRoute(
+                name: "Accueil",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Accueil", action = "Index", id = UrlParameter.Optional }
+            );
+
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
---
+++ Touristix/Controllers/AccueilController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Touristix.Controllers
+{
+    public class AccueilController : Controller
+    {
+        //
+        // GET: /Accueil/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
